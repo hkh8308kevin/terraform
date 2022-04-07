14e54aaf2a69c8ab5daf08c0fb2187beda923f77
hkh8308kevin <hkh8308@gmail.com>
2022-04-07 17:50:00 +0900

    add load balancer

--- main.tf
+++ main.tf
@@ -29,10 +29,30 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port = 80
+    to_port =80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_autoscaling_group" "WEB-ASG" {
   name = "terraform-web-asg"
   launch_configuration = aws_launch_configuration.launch_config.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.tg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 3
@@ -44,9 +64,71 @@ resource "aws_autoscaling_group" "WEB-ASG" {
   } 
 }
 
+resource "aws_lb" "example" {
+  name = "terraform-lb-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  default_action {
+   type = "fixed-response"
+   
+   fixed_response {
+    content_type = "text/plain"
+    message_body = "404: page not found"
+    status_code = 404
+   }
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name = "terraform-tg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path =  "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+   path_pattern {
+     values = ["*"]
+   }
+  }
+  
+  action {
+   type = "forward"
+   target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 data "aws_vpc" "default" {
   default = true
 }
+
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load blancer"
+}
+
