a515a814ee864e6964a7b1833362b3d86bcfffd0
hkh8308kevin <hkh8308@gmail.com>
2022-04-09 01:46:09 +0900

    add backend

---
+++ backend/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+   bucket = "terraform-up-and-running-state-kevin"
+   key    = "global/s3/terraform.tfstate"
+   region = "ap-northeast-2"
+
+   dynamodb_table = "terraform-up-and-running-locks"
+   encrypt        = "true"
+   }
+}
+
